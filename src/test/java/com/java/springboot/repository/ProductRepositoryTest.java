b7975298457c1f374db1f1c3ee5cfdfddf050ac9
shivamK98 <shivam.kumar@connectandheal.com>
2022-06-06 15:18:06 +0530

    remove unnescessary imports

--- src/test/java/com/java/springboot/repository/ProductRepositoryTest.java
+++ src/test/java/com/java/springboot/repository/ProductRepositoryTest.java
@@ -1,8 +1,6 @@
 package com.java.springboot.repository;
 
 import com.java.springboot.model.ProductDTO;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
